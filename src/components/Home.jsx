eccee375f9c9393d43ea1d5c4e23b16d5a0b9775
Josh <joshuamace@gmail.com>
2024-04-05 10:11:16 -0500

    fixing images changed moon  url

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,11 +9,11 @@ const Home = () => {
           <div className='person__container'>
             <img
               className='person__circle'
-              src='JMace88.github.io/build/assets/moon-png-44668.png'
+              src='moon-png-44668.png'
             />
             <img
               className='person__img'
-              src='/assets/images/with yuki no background.png'
+              src='withyukinobackground.png'
               alt='picture for home'
             />
           </div>
