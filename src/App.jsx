c4e2ca9f7f8390609fe2cc4e24cfc71207504e5e
Josh <joshuamace@gmail.com>
2024-04-05 10:35:06 -0500

    added hashrouter to avoid 404 error

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Route path='/about' element={<AboutMe />} />
         <Route path='/contact' element={<ContactMe />} />
         <Route path='/portfolio' element={<Portfolio />} />
-        <Route path='*' element= {<Home />} />
+        {/* <Route path='*' element= {<Home />} /> */}
       </Routes>
     </>
   );
