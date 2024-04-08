35ece11318d3f0caaafbb8836ea34766d6e40e23
Josh <joshuamace@gmail.com>
2024-04-08 18:27:40 -0500

    media formatting completed for home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,22 +3,23 @@ import './styles/home.css';
 const Home = () => {
   return (
     <>
-      <main>
-        <div className='person'>
-          <h1>Josh Mace</h1>
-          <div className='person__container'>
-            <img
-              className='person__circle'
-              src='moon-png-44668.png'
-            />
-            <img
-              className='person__img'
-              src='withyukinobackground.png'
-              alt='picture for home'
-            />
+      <main className='home-main'>
+        <div className='all-items'>
+            <div className='person'>
+              <div className='person__container'>
+                <img className='person__circle' src='crabnebula.jpg' />
+                <img
+                  className='person__img'
+                  src='withyukinobackground.png'
+                  alt='picture for home'
+                />
+              </div>
+            </div>
+          <div >
+            <h1 className='yuki'>Josh</h1>
+            <h1 className='yuki'>& Yuki!</h1>
           </div>
         </div>
-        <h1 className='yuki'>and Yuki!</h1>
       </main>
     </>
   );
